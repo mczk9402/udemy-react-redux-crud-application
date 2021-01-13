352005710f093fd5f12336b73c81302841e02e46
mczk9402 <47809450+mczk9402@users.noreply.github.com>
2021-01-13 22:29:55 +0900

    fix typo for Provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
-import { provider } from 'react-redux'
+import { Provider } from 'react-redux'
 
 import './index.css';
 import reducer from './reducers'
